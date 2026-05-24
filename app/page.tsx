cee8443a5628cc03a4889a80a5cc39d9d4bab43b
Matthew Robinson <matthewrobinson@Mac.home>
2026-05-24 08:36:49 +0200

    Route home and service pages to full Apriil digital landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,5 @@
-import { HeroSection } from "../components/hero-section";
-import { ProblemSection } from "../components/problem-section";
-import { ServiceCards } from "../components/service-cards";
-import { OperationalFlow } from "../components/operational-flow";
-import { PricingSection } from "../components/pricing-section";
-import { ResultsSection } from "../components/results-section";
-import { AboutSection } from "../components/about-section";
-import { ContactSection } from "../components/contact-section";
+import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  return (
-    <main>
-      <HeroSection />
-      <ProblemSection />
-      <ServiceCards />
-      <OperationalFlow />
-      <PricingSection />
-      <ResultsSection />
-      <AboutSection />
-      <ContactSection />
-    </main>
-  );
+  redirect("/apriil-digital/index.html");
 }
